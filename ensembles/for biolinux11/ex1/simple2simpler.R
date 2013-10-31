4328e560bf636a5ec0326125346419469dce9c9c
kojitominaga <koji.tominaga@ibv.uio.no>
2013-10-31 13:36:02 +0100

    simple to simpler (rLakeAnalyzer) seems to work

--- ensembles/for biolinux11/ex1/simple2simpler.R
+++ ensembles/for biolinux11/ex1/simple2simpler.R
@@ -11,24 +11,35 @@ for (di in 1:length(dnames)) {
     next
   }
   dm <- read.csv(dmfname)
+
   cat('Calculating Lake Analyzer indicators\n')
+  m <- 24
+  parameters <- c(10, 1000, 0.5,
+                  NA, ## this will be fine for this application only
+                  sin(60 / 180 * pi) * 2 * 7.29e-5,
+                  60 * 60 * 24 / m,
+                  0, 0.8, 0.8)
+  names(parameters) <-
+    c('depth_w', 'fetch', 'depth_bs', 'T_bs', 'par_Coriolis', 'del_time',
+      'albedo_water', 'albedo_ice', 'albedo_snow')
   depths <- seq(from = 0, to = parameters[['depth_w']], by = 0.1)
   bthA <- (1000 * (parameters[['depth_w']] - depths) /
            parameters[['depth_w']]) ^ 2 * pi
+
   thislais <-
     tryCatch({
-      LAIndices(result0, parameters, bthA, depths)
+      LAIndices(dm, parameters, bthA, depths)
       }, error = function(msg) {
         f <- file('errors', open = 'a')
         cat(c(dname, ':', '\n', str(msg), '--\n'),
             file = f)
         cat('failed to calculate Lake Analyzer Indices\n--\n', file = f)
         close(f)
-        thislais <- data.frame(ThermoclineDepth = rep(NA, times = nrow(result0)),
-                               MixedLayerDepth = rep(NA, times = nrow(result0)),
-                               SchmidtStability = rep(NA, times = nrow(result0)),
-                               WedderburnNumber = rep(NA, times = nrow(result0)),
-                               LakeNumber = rep(NA, times = nrow(result0))
+        thislais <- data.frame(ThermoclineDepth = rep(NA, times = nrow(dm)),
+                               MixedLayerDepth = rep(NA, times = nrow(dm)),
+                               SchmidtStability = rep(NA, times = nrow(dm)),
+                               WedderburnNumber = rep(NA, times = nrow(dm)),
+                               LakeNumber = rep(NA, times = nrow(dm))
                                )
         return(thislais)
       }, warning = function(msg) {
@@ -38,8 +49,20 @@ for (di in 1:length(dnames)) {
         close(f)
         return(thislais)
       })
+  thislais[['ThermoclineDepth']] <-
+    round(thislais[['ThermoclineDepth']], digits = 3)
+  thislais[['MixedLayerDepth']] <-
+    round(thislais[['MixedLayerDepth']], digits = 3)
+  thislais[['SchmidtStability']] <-
+    signif(thislais[['SchmidtStability']], digits = 4)
+  thislais[['WedderburnNumber']] <-
+    signif(thislais[['WedderburnNumber']], digits = 4)
+  thislais[['LakeNumber']] <-
+    signif(thislais[['LakeNumber']], digits = 4)
+  
   laisfname <- paste0(dname, '/csv/dm_simple_lais.csv.xz')
   laisf <- xzfile(laisfname, open = 'wb')
   write.csv(thislais, file = laisf)
+  close(laisf)
 }
 
