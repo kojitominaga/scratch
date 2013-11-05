dd0df6cc2be598c21f677357e836cacca43eb210
kojitominaga <koji.tominaga@ibv.uio.no>
2013-11-05 15:12:52 +0100

    made new for depth comparison

---
+++ ensembles/for biolinux11/ex1/rundepthcomparison.R
@@ -0,0 +1,227 @@
+require(flakeutil)
+
+## runs FLake and does not calculate LakeAnalyzer indices
+## (wait for any development there)
+
+runbydepth <- function(dirname, thisdepth) {
+  cat(dirname)
+  cat('\n')
+  fnameaffix <- 'simple'
+
+  dirname2 <- paste0(dirname, '/', as.character(thisdepth), '/')
+
+  if (!file.exists(dirname2)) dir.create(dirname2)
+
+  firstday <- as.Date('1991-01-01')
+  lastday <- as.Date('2000-12-31')
+  dayseq <- seq(from = firstday, to = lastday, by = 'day')
+  dayseqP <- as.POSIXlt(dayseq)
+
+  pr <- scan(paste0(dirname, '/', 'pr.csv.gz'))
+  rlds <- scan(paste0(dirname, '/', 'rlds.csv.gz'))
+  wss <- scan(paste0(dirname, '/', 'wss.csv.gz'))
+  huss <- scan(paste0(dirname, '/', 'huss.csv.gz'))
+  ps <- scan(paste0(dirname, '/', 'ps.csv.gz'))
+
+  rootpath24 <- './'
+  rsds24 <- scan(paste0(dirname, '/', 'rsds24.csv.gz'))
+  tas24 <- scan(paste0(dirname, '/', 'tas24.csv.gz'))
+  
+  height_u <- 10
+  height_tq <- 2
+  
+  n <- length(pr)
+  m <- 24
+  initialrepeat <- 2
+  
+  ts.input0 <- data.frame(dMsnowdt_in = rep(pr, each = m),
+                          I_atm_in = rsds24,
+                          Q_atm_lw_in = rep(rlds, each = m),
+                          height_u_in = rep(rep(height_u, times = n),
+                            each = m),
+                          height_tq_in = rep(rep(height_tq, times = n),
+                            each = m),
+                          U_a_in = rep(wss, each = m),
+                          T_a_in = tas24,
+                          q_a_in = rep(huss, each = m),
+                          P_a_in = rep(ps, each = m)
+                          )
+  if(any(is.na(as.matrix(ts.input0)))) {
+    skippedf <- file("skipped", open = 'a')
+    cat(c("skipped:", dirname, '\n'), file = skippedf)
+    close(skippedf)
+    next
+    ## this is necessary when something went wrong with the input
+    ## meteorological data. Just skip it. 
+  }
+  ts.input <- ts.input0[c(rep(1:(m * 365), times = initialrepeat), 1:(n * m)), ]
+  initiali <- 1:(m * 365 * initialrepeat)
+  
+  parameters <- c(thisdepth, 1000, 0.5,
+                  mean(ts.input[['T_a_in']]),
+                  sin(60 / 180 * pi) * 2 * 7.29e-5,
+                  60 * 60 * 24 / m,
+                  0, 0.8, 0.8)
+  names(parameters) <-
+    c('depth_w', 'fetch', 'depth_bs', 'T_bs', 'par_Coriolis', 'del_time',
+      'albedo_water', 'albedo_ice', 'albedo_snow')
+  
+  init.state <-
+    c(0 + 273.15, 0 + 273.15, 4 + 273.15, 4 + 273.15, 4 + 273.15, 5 + 273.15,
+      0.5, 0, 0, parameters[['depth_w']], 0.4, 4 + 273.15)
+  names(init.state) <-
+    c('T_snow', 'T_ice', 'T_mnw', 'T_wML', 'T_bot', 'T_B1',
+      'C_T', 'h_snow', 'h_ice', 'h_ML', 'H_B1', 'T_sfc')
+  
+  optic <- c(2, 20, 20)
+  names(optic) <- paste('extincoef_', c('water', 'ice', 'snow'), sep = '')
+
+  cat('Running FLake\n')
+  result0 <- data.frame(ts.input,
+                        runflake(ts.input, parameters, init.state, optic)
+                        )[-initiali, ]
+  ## depths <- seq(from = 0, to = parameters[['depth_w']], by = 0.1)
+  ## bthA <- (1000 * (parameters[['depth_w']] - depths) /
+  ##          parameters[['depth_w']]) ^ 2 * pi
+  ## cat('Calculating Lake Analyzer indicators\n')
+  ## thislai <-
+  ##   tryCatch({
+  ##     LAIndices(result0, parameters, bthA, depths)
+  ##     }, error = function(msg) {
+  ##       f <- file('errors', open = 'a')
+  ##       cat(c(folder, ':', '\n', str(msg), '--\n'),
+  ##           file = f)
+  ##       cat('failed to calculate Lake Analyzer Indices\n--\n', file = f)
+  ##       close(f)
+  ##       thislai <- data.frame(ThermoclineDepth = rep(NA, times = nrow(result0)),
+  ##                             MixedLayerDepth = rep(NA, times = nrow(result0)),
+  ##                             SchmidtStability = rep(NA, times = nrow(result0)),
+  ##                             WedderburnNumber = rep(NA, times = nrow(result0)),
+  ##                             LakeNumber = rep(NA, times = nrow(result0))
+  ##                             )
+  ##       return(thislai)
+  ##     }, warning = function(msg) {
+  ##       f <- file('warnings', open = 'a')
+  ##       cat(c(folder, ':', dayi, print(day), '\n', str(msg), '--\n'),
+  ##           file = f)
+  ##       close(f)
+  ##       return(thislai)
+  ##     })
+  result <- result0
+  fullnames2 <- c(fullnames ## ,
+                  ## c('Thermocline depth',
+                  ##   'Mixed layer depth',
+                  ##   'Schmidt stability',
+                  ##   'Wederburn number',
+                  ##   'Lake number'
+                  ##   )
+                  )
+    
+  cat('Calculating daily mean\n')
+  dm <- apply(result, 2, tapply,
+              rep(1:(n * m), each = 24, length.out = nrow(result)),
+              mean)
+
+  cat('Writing out files\n')  
+  
+  if (!file.exists(paste0(dirname2, '/csv'))) dir.create(paste0(dirname2, '/csv'))
+  
+  c <- bzfile(paste0(dirname2, '/csv/dm_', fnameaffix, '.csv.bz2'),
+              open = 'wb')
+  write.csv(data.frame(date = dayseq, signif(dm, digits = 4)), file = c)
+  close(c)
+  
+  typicalyear <- apply(dm, 2, tapply, dayseqP$yday, mean)[1:365, ]
+  ## 1:365 makes it ignore 31 Dec in leap years
+
+  c <- bzfile(paste0(dirname2, '/csv/typicalyear_', fnameaffix, '.csv.bz2'),
+              open = 'wb')
+  write.csv(data.frame(MonthDay = format(seq(from = as.Date('1999-01-01'),
+                         to = as.Date('1999-12-31'),
+                         by = 'day'), ## need any non-leap year
+                         format = '%m-%d'),
+                       signif(typicalyear, digits = 4)), 
+            file = c)
+  close(c)
+  
+  ## shortnames <- names(result)
+  ## if (!file.exists(paste0(dirname, '/pdf'))) dir.create(paste0(dirname, '/pdf'))
+  ## pdf(paste0(dirname, '/pdf/allvariables_dm_', fnameaffix, '.pdf'), 
+  ##     height = 7.5, width = 11.5)
+  ## par(mfcol = c(3, 3))
+  ## par(mar = c(2, 3, 3, 1))
+  ## for (vi in 1:ncol(typicalyear)) {
+  ##   plot(seq(from = as.Date('1999-01-01'),
+  ##            to = as.Date('1999-12-31'),
+  ##            by = 'day'),
+  ##        typicalyear[1:365, vi],
+  ##        type = 'l',
+  ##        xlab = 'month',
+  ##        ylab = '',
+  ##        main = fullnames2[vi],
+  ##        cex.main = 1,
+  ##        xaxt = 'n')
+  ##   axis.Date(1,
+  ##             at = seq(from = as.Date('1999-01-01'),
+  ##               to = as.Date('1999-12-01'),
+  ##               by = 'month'),
+  ##             labels = c('J', 'F', 'M', 'A', 'M', 'J',
+  ##               'J', 'A', 'S', 'O', 'N', 'D')
+  ##             )
+  ## }
+  ## dev.off()
+  return(NULL)
+}  
+
+## main loops
+folders <- list.dirs()
+depths <- c(2, 5, 10, 20, 30, 40)
+for (fi in 1:length(folders)) {
+  temp <- strsplit(folders[fi], '/')[[1]]
+  folder <- temp[length(temp)]
+  temp2 <- strsplit(folder, '-')[[1]]
+  x <- as.integer(temp2[1])
+  y <- as.integer(temp2[2])
+  if (is.na(x)) {
+    ## if 'x' is not number, e.g., './' -- it is not a folder like '093-082'
+    cat('-------------------------\n')
+    cat('skipping the folder ')
+    cat(folder)
+    cat('\n-------------------------\n')
+    next
+  } else if (x < 38 | x > 42 | y < 67 | y > 71) {
+    cat('-------------------------\n')
+    cat('skipping the folder ')
+    cat(folder)
+    cat('\n-------------------------\n')
+    next
+  }
+  if (file.exists(paste0(folder, '/csv/dm_simple.csv.xz'))) {
+    cat('-------------------------\n')
+    cat('skipping the folder ')
+    cat(folder)
+    cat('because it is already finished\n-------------------------\n')
+    next
+  }
+  cat('-- Running the folder ')
+  cat(folder)
+  cat(' --\n')
+  for (di in 1:length(depths)) {
+    thisdepth <- depths[di]
+    cat(paste('-- Running depth', thisdepth, '\n'))
+    tryCatch({
+      runbydepth(folder, thisdepth)
+    }, error = function(msg) {
+      f <- file('errors_main_loop', open = 'a')
+      cat(c(folder, ':', '\n', str(msg), '--\n'), file = f)
+      close(f)
+      return(NULL)
+    }, warning = function(msg) {
+      f <- file('warnings_main_loop', open = 'a')
+      cat(c(folder, ':', '\n', str(msg), '--\n'), file = f)
+      close(f)
+      return(NULL)
+    })
+  }
+}
+  
