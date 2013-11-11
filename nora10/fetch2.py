ba8071d33b02fd453c7d2dbe76db1dc66b69f5ad
kojitominaga <koji.tominaga@ibv.uio.no>
2013-11-11 10:42:46 +0800

    NORA10 file get scripts

---
+++ nora10/fetch2.py
@@ -0,0 +1,19 @@
+"""fetch from ENSEMBLES RT3"""
+import urllib
+
+## testing with the ERA40 simulation
+address1 = "ftp://ftp.met.no/projects/nora10/NORA10_1H_11km_pr_"
+address2 = range(1958, 2013)
+address3 = ".nc"
+
+addresses = ['%s%s%s' % (address1, address2[i], address3) 
+             for i in range(len(address2))]
+
+f = open('fetch.sh', 'w')
+out = '\n'.join(['wget %s' % a for a in addresses])
+f.write(out)
+f.close()
+
+
+    
+
