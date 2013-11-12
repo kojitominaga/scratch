16e5b22e15f8aa70898f12fe0c01a4c3c4c16301
kojitominaga <koji.tominaga@ibv.uio.no>
2013-11-12 09:37:22 +0800

    Now it works

--- nora10/testintegrity.py
+++ nora10/testintegrity.py
@@ -12,14 +12,15 @@ out = '\n'.join(fnames)
 f.write(out)
 f.write('\n')
 
-f2 = open('ranges.csv', 'w')
-f2.write('file,variable,min,max\n')
+f2 = open('ranges.csv', 'a')
+f2.write('file,variable,di,min,max\n')
 
 for g in fnames:
     try: 
         r = netCDF4.Dataset(g)
     except RuntimeError:
         f.write('error in: ' + g + '\n')
+        continue
     else:
         sys.stdout = open(g + '.meta', 'w')
         print(r)
@@ -32,14 +33,23 @@ for g in fnames:
                 continue 
             if nd == 1:
                 temp = r.variables[v][:]
+                temp2 = 'NA'
+                f2.write('%s,%s,%s,%f,%f\n' % (g, v, temp2, temp.min(), temp.max()))
             if nd == 2:
-                temp = r.variables[v][:, :].\
-                  reshape(np.product(r.variables[v].shape), )
+                temp = r.variables[v][:, :].reshape(np.product(r.variables[v].shape), )
+                temp2 = 'NA'
+                f2.write('%s,%s,%s,%f,%f\n' % (g, v, temp2, temp.min(), temp.max()))
             if nd == 3:
-                temp = r.variables[v][:, :, :].\
-                  reshape(np.product(r.variables[v].shape), )
+                print r.variables[v].shape
+                nx = r.variables[v].shape[0]
+                dayii = [int(n) for n in [0] + np.floor(np.random.random(3) * nx).tolist() + [nx - 1]]
+                for dayi in dayii:
+                    temp = r.variables[v][dayi, :, :].flatten()
+                    f2.write('%s,%s,%s,%f,%f\n' % (g, v, dayi, temp.min(), temp.max()))
             if nd == 4:
-                temp = r.variables[v][:, :, :, :].\
-                  reshape(np.product(r.variables[v].shape), )
-            f2.write('%s,%s,%f,%f\n' % (g, v, temp.min(), temp.max()))
+                nx = r.variables[v].shape[0]
+                dayii = [int(n) for n in [0] + np.floor(np.random.random(3) * nx).tolist() + [nx - 1]]
+                for dayi in dayii:
+                    temp = r.variables[v][dayi, :, :, :].flatten()
+                    f2.write('%s,%s,%s,%f,%f\n' % (g, v, dayi, temp.min(), temp.max()))
         r.close()
