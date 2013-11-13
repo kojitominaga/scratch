a96a8d033595a6b6865d4a3f3c05e6cedc18d128
kojitominaga <koji.tominaga@ibv.uio.no>
2013-11-13 08:50:58 +0800

    finished a working version

--- utilities/nc2latlon/nc2latlon.py
+++ utilities/nc2latlon/nc2latlon.py
@@ -1,4 +1,6 @@
 """usage: python nc2latlon.py yournetcdffile.nc"""
+## x lon EW
+## y lat NS
 
 import netCDF4
 import sys
@@ -14,12 +16,26 @@ lat = r.variables['lat'][:, :] # y * x
 nx = r.variables['rlon'].size  # x
 ny = r.variables['rlat'].size  # y
 
-np.savetxt(ncf + '.lon.txt', lon) 
-np.savetxt(ncf + '.lat.txt', lat)
+np.savetxt(ncf + '.lon.txt', lon.transpose(), fmt = '%.6f') # x * y
+np.savetxt(ncf + '.lat.txt', lat.transpose(), fmt = '%.6f') # x * y
 
 # x (lon) moves fastest
-xii = range(nx) * ny
+xii = range(nx) * ny 
+# x0, x1, x2, x3, x4, ...
 yii = [yi for yi in range(ny) for _ in range(nx)]
-lon1d = lon.transpose().reshape((nx * ny,)
-lat1d = lat.reshape((nx * ny, ))
+# y0, y0, y0, y0, y0, ...
+lon1d = lon.reshape((nx * ny, )) # x0y0, x1y0, x2y0, x3y0, ... 
+lat1d = lat.reshape((nx * ny, )) # x0y0, x1y0, x2y0, x3y0, ... 
+f1out = '\n'.join(['%s-%s %.6f %.6f' % (xii[i], yii[i], lon1d[i], lat1d[i])
+                   for i in range(len(xii))])
+f2out = '\n'.join(['%s-%s %.6f %.6f' % (xii[i], yii[i], lat1d[i], lon1d[i])
+                   for i in range(len(xii))])
+f1 = open(ncf + '.lonlat.txt', 'w')
+f1.write(f1out)
+f1.close()
+f2 = open(ncf + '.latlon.txt', 'w')
+f2.write(f2out)
+f2.close()
+
+    
 
