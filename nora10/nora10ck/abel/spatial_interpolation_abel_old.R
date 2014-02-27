7154e42b3994f7376214ca5bfbf92ef7cad3cbfa
kojitominaga <koji.tominaga@ibv.uio.no>
2014-02-27 11:33:05 +0800

    tidied up the R script a bit

--- nora10/nora10ck/abel/spatial_interpolation_abel_old.R
+++ nora10/nora10ck/abel/spatial_interpolation_abel_old.R
@@ -101,6 +101,19 @@ interpolated <- lapply(1:nlakes, function(x) rep(NA, times = ninterpmethods))
 nmetadata <- 33
 metadata <- lapply(1:nlakes, function(x) rep(NA, times = nmetadata))
 
+## if (!file.exists('interpolated')) dir.create('interpolated')
+## if (!file.exists('interpolated/vario')) dir.create('interpolated/vario')
+## if (!file.exists('interpolated/pred')) dir.create('interpolated/pred')
+## if (!file.exists(paste0('interpolated/vario/', varname))) {
+##   dir.create(paste0('interpolated/vario/', varname))
+## }
+## if (!file.exists(paste0('interpolated/pred/', varname))) {
+##   dir.create(paste0('interpolated/pred/', varname))
+## }
+## if (!file.exists(paste0('interpolated/vario/', varname, '/', year))) {
+##   dir.create(paste0('interpolated/vario/', varname, '/', year))
+## }
+
 
 ####### looping for individual interpolation #######
 for (theindex in 1:nn) {
@@ -129,6 +142,8 @@ ranks <- lapply(as.list(1:nlakes),
                 function(lakei) {
                   rank(spDistsN1(n10df, lakes[lakei,], longlat = TRUE))
                 })
+## ## scope 2 SEE bb1
+## n10pan <- n10df[bb1, ]
 
 ## if the nearest 5 has the same value:
 ##    i1a, i1b, i1c, i3an, i3ao, i3bn, i3bo, i3cn, i3co 
@@ -212,6 +227,14 @@ i1c <-
                   return(out)
                 }))
 
+## ## scope 2 variogram (pan regional) SEE bb1
+## if (completelyhomog2) {
+##   v2 <- NA
+##   v2o <- NA
+## } else {
+##   v2 <- variogram(v ~ 1, n10pan, cutoff = ncutoff)
+##   v2o <- variogram(v ~ orog, n10pan, cutoff = ncutoff)
+## }
 ## scope 3 variogram
 n10sub <- lapply(as.list(1:nlakes),
                  function(lakei) {
@@ -229,6 +252,9 @@ v3 <- lapply(as.list(1:nlakes),
                }
                return <- out
              })
+cat('done v3, now starting v3o\n')
+cat(format(Sys.time(), "%a %b %d %X %Y"))
+cat('\n')
 v3o <- lapply(as.list(1:nlakes),
               function(lakei) {
                 if (locallyhomog3[[lakei]]) {
@@ -242,7 +268,9 @@ cat('done v3o\n')
 cat(format(Sys.time(), "%a %b %d %X %Y"))
 cat('\n')
 
-## fitted variograms
+## fitted variograms (scopes 2 and 3)
+## maxgamma2 <- ifelse(completelyhomog2, NA, max(v2[['gamma']]))
+## maxgamma2o <- ifelse(completelyhomog2, NA, max(v2o[['gamma']]))
 maxgamma3 <- lapply(as.list(1:nlakes),
                     function(lakei) {
                       if (locallyhomog3[[lakei]]) {
@@ -261,6 +289,8 @@ maxgamma3o <- lapply(as.list(1:nlakes),
                        }
                        return(out)
                      })
+## maxdist2 <- ifelse(completelyhomog2, NA, max(v2[['dist']]))
+## maxdist2o <- ifelse(completelyhomog2, NA, max(v2o[['dist']]))
 maxdist3 <- lapply(as.list(1:nlakes),
                    function(lakei) {
                      if (locallyhomog3[[lakei]]) {
@@ -279,6 +309,15 @@ maxdist3o <- lapply(as.list(1:nlakes),
                       }
                       return(out)
                     })
+## if (completelyhomog2) {
+##   vfl2 <- NA
+##   vfl2o <- NA
+## } else {
+##   vfl2 <- fit.variogram(v2, vgm(maxgamma2, 'Lin', maxdist2),
+##                         fit.sills = TRUE, fit.ranges = FALSE)
+##   vfl2o <- fit.variogram(v2o, vgm(maxgamma2o, 'Lin', maxdist2),
+##                          fit.sills = TRUE, fit.ranges = FALSE)
+## }
 vfl3 <- lapply(as.list(1:nlakes),
                function(lakei) {
                  if (locallyhomog3[[lakei]]) {
@@ -305,6 +344,29 @@ vfl3o <- lapply(as.list(1:nlakes),
                   }
                   return(out)
                 })
+## if (completelyhomog2) {
+##   vfe2 <- NA
+##   vfe2o <- NA
+## } else {
+##   vfe2 <-
+##     fit.variogram(v2,
+##                   vgm(maxgamma2 / 2, 'Sph', maxdist2 * 0.5,
+##                       add.to = vgm(maxgamma2 / 2, 'Sph', maxdist2 * 0.75, 
+##                         add.to = vgm(maxgamma2 / 2, 'Exp', maxdist2,
+##                           add.to = vgm(maxgamma2 / 2, 'Gau', maxdist2,
+##                             add.to = vgm(maxgamma2 / 2, 'Lin', maxdist2,
+##                               maxgamma2 / 2))))),
+##                   fit.sills = TRUE, fit.ranges = FALSE)
+##   vfe2o <- 
+##     fit.variogram(v2o,
+##                   vgm(maxgamma2o / 2, 'Sph', maxdist2o * 0.5,
+##                       add.to = vgm(maxgamma2o / 2, 'Sph', maxdist2o * 0.75, 
+##                         add.to = vgm(maxgamma2o / 2, 'Exp', maxdist2o,
+##                           add.to = vgm(maxgamma2o / 2, 'Gau', maxdist2o,
+##                             add.to = vgm(maxgamma2o / 2, 'Lin', maxdist2o,
+##                               maxgamma2o / 2))))),
+##                   fit.sills = TRUE, fit.ranges = FALSE)
+## }
 vfe3 <-
   lapply(as.list(1:nlakes),
          function(lakei) {
@@ -351,7 +413,16 @@ vfe3o <-
              }
              return(out)
            })
-## 3an* : inverse distance
+## 2an*, 3an* : inverse distance
+## if (completelyhomog2) {
+##   k2an <- NA
+##   i2an <- homog2
+##   i2an.var <- rep(NA, times = nlakes)
+## } else {
+##   k2an <- krige(v ~ 1, n10pan, lakes, nmax = nlocal)
+##   i2an <- k2an[['var1.pred']]
+##   i2an.var <- k2an[['var1.var']]
+## }
 k3an <-
   lapply(as.list(1:nlakes),
            function(lakei) {
@@ -381,7 +452,16 @@ i3an.var <- unlist(lapply(as.list(1:nlakes),
                             }
                             return(out)
                           }))
-## 3ao* : inverse distance with weighting by orog
+## 2ao*, 3ao* : inverse distance with weighting by orog
+## if (completelyhomog2) {
+##   k2ao <- NA
+##   i2ao <- homog2
+##   i2ao.var <- rep(NA, times = nlakes)
+## } else {
+##   k2ao <- krige(v ~ orog, n10pan, lakes, nmax = nlocal)
+##   i2ao <- k2ao[['var1.pred']]
+##   i2ao.var <- k2ao[['var1.var']]
+## }
 k3ao <-
   lapply(as.list(1:nlakes),
          function(lakei) {
@@ -411,7 +491,16 @@ i3ao.var <- unlist(lapply(as.list(1:nlakes),
                             }
                             return(out)
                           }))
-## 3bn* : kriging (linear variogram model), ordinary
+## 2bn*, 3bn* : kriging (linear variogram model), ordinary
+## if (completelyhomog2) {
+##   k2bn <- NA
+##   i2bn <- homog2
+##   i2bn.var <- rep(NA, times = nlakes)
+## } else {
+##   k2bn <- krige(v ~ 1, n10pan, lakes, vfl2, nmax = nlocal)
+##   i2bn <- k2bn[['var1.pred']]
+##   i2bn.var <- k2bn[['var1.var']]
+## }
 k3bn <-
   lapply(as.list(1:nlakes),
          function(lakei) {
@@ -441,7 +530,16 @@ i3bn.var <- unlist(lapply(as.list(1:nlakes),
                             }
                             return(out)
                           }))
-## 3bo* : kriging (linear variogram model), universal
+## 2bo*, 3bo* : kriging (linear variogram model), universal
+## if (completelyhomog2) {
+##   k2bo <- NA
+##   i2bo <- homog2
+##   i2bo.var <- rep(NA, times = nlakes)
+## } else {
+##   k2bo <- krige(v ~ orog, n10pan, lakes, vfl2o, nmax = nlocal)
+##   i2bo <- k2bo[['var1.pred']]
+##   i2bo.var <- k2bo[['var1.var']]
+## } 
 k3bo <-
   lapply(as.list(1:nlakes),
          function(lakei) {
@@ -471,7 +569,16 @@ i3bo.var <- unlist(lapply(as.list(1:nlakes),
                             }
                             return(out)
                           }))
-## 3cn* : kriging (complex variogram model), ordinary
+## 2cn*, 3cn* : kriging (complex variogram model), ordinary
+## if (completelyhomog2) {
+##   k2cn <- NA
+##   i2cn <- homog2
+##   i2cn.var <- rep(NA, times = nlakes)
+## } else {
+##   k2cn <- krige(v ~ 1, n10pan, lakes, vfe2, nmax = nlocal)
+##   i2cn <- k2cn[['var1.pred']]
+##   i2cn.var <- k2cn[['var1.var']]
+## }
 k3cn <-
   lapply(as.list(1:nlakes),
          function(lakei) {
@@ -501,7 +608,16 @@ i3cn.var <- unlist(lapply(as.list(1:nlakes),
                               }
                             return(out)
                           }))
-## 3co* : kriging (complex variogram model), universal
+## 2co*, 3co* : kriging (complex variogram model), universal
+## if (completelyhomog2) {
+##   k2co <- NA
+##   i2co <- homog2
+##   i2co.var <- rep(NA, times = nlakes)
+## } else {
+##   k2co <- krige(v ~ orog, n10pan, lakes, vfe2o, nmax = nlocal)
+##   i2co <- k2co[['var1.pred']]
+##   i2co.var <- k2co[['var1.var']]
+## }
 k3co <-
   lapply(as.list(1:nlakes),
          function(lakei) {
@@ -532,10 +648,25 @@ i3co.var <- unlist(lapply(as.list(1:nlakes),
                             return(out)
                           }))
 cat(format(Sys.time(), "%a %b %d %X %Y"))
-cat('-- krigin done \n')
+cat('\n')
 ## finally putting the calculated values in container
 for (lakei in 1:nlakes) {
   lakename <- lakes[['name']][lakei]
+  ## if (!file.exists(sprintf('%s/%s', outdir, lakename))) {
+  ##   dir.create(sprintf('%s/%s', outdir, lakename))
+  ## }
+  ## if (!file.exists(sprintf('%s/%s/%s', outdir, lakename, varname))) {
+  ##   dir.create(sprintf('%s/%s/%s', outdir, lakename, varname))
+  ## }
+  ## if (!file.exists(sprintf('%s/%s/%s/pred', outdir, lakename, varname))) {
+  ##   dir.create(sprintf('%s/%s/%s/pred', outdir, lakename, varname))
+  ## }
+  ## if (!file.exists(sprintf('%s/%s/%s/meta', outdir, lakename, varname))) {
+  ##   dir.create(sprintf('%s/%s/%s/meta', outdir, lakename, varname))
+  ## }
+  ## if (!file.exists(sprintf('%s/%s/%s/vario', outdir, lakename, varname))) {
+  ##   dir.create(sprintf('%s/%s/%s/vario', outdir, lakename, varname))
+  ## }
   if (!file.exists(sprintf('%s/pred', outdir))) {
     dir.create(sprintf('%s/pred', outdir))
   }
@@ -554,6 +685,12 @@ for (lakei in 1:nlakes) {
   interpolated <- c(i1a[lakei],
                     i1b[lakei],
                     i1c[lakei],
+                    ## i2an[lakei],
+                    ## i2ao[lakei],
+                    ## i2bn[lakei],
+                    ## i2bo[lakei],
+                    ## i2cn[lakei],
+                    ## i2co[lakei],
                     i3an[lakei],
                     i3ao[lakei],
                     i3bn[lakei],
@@ -563,37 +700,71 @@ for (lakei in 1:nlakes) {
   cat(interpolated, file = predfn)
   cat('\n', file = predfn, append = TRUE)
   metadata <-
-    c(locallyhomog3[[lakei]] * 1,
+    c(
+      ## completelyhomog2 * 1,
+      locallyhomog3[[lakei]] * 1,
+      ## maxgamma2,
+      ## maxgamma2o,
       maxgamma3[[lakei]],
       maxgamma3o[[lakei]],
+      ## maxdist2,
+      ## maxdist2o,
       maxdist3[[lakei]],
       maxdist3o[[lakei]],
+      ## round(ifelse(completelyhomog2, NA, vfl2[['psill']]) /
+      ##       maxgamma2, digits = 3),
+      ## round(ifelse(completelyhomog2, NA, vfl2o[['psill']]) /
+      ##       maxgamma2o, digits = 3),
       round(ifelse(locallyhomog3[[lakei]], NA, vfl3[[lakei]][['psill']]) /
             maxgamma3[[lakei]], digits = 3),
       round(ifelse(locallyhomog3[[lakei]], NA, vfl3o[[lakei]][['psill']]) /
             maxgamma3o[[lakei]], digits = 3),
+      ## round(ifelse(rep(completelyhomog2, times = 6),
+      ##              rep(NA, times = 6), vfe2[['psill']]) /
+      ##       maxgamma2, digits = 3), ## 6 elements
+      ## round(ifelse(rep(completelyhomog2, times = 6),
+      ##              rep(NA, times = 6), vfe2o[['psill']]) /
+      ##       maxgamma2o, digits = 3), ## 6 elements
       round(ifelse(rep(locallyhomog3[[lakei]], times = 6),
                    rep(NA, times = 6), vfe3[[lakei]][['psill']]) /
             maxgamma3[[lakei]], digits = 3), ## 6 elements
       round(ifelse(rep(locallyhomog3[[lakei]], times = 6),
                    rep(NA, times = 6), vfe3o[[lakei]][['psill']]) /
             maxgamma3o[[lakei]], digits = 3),  ## 6 elements
+      ## ifelse(completelyhomog2, NA, attr(vfl2, 'SSErr')),
+      ## ifelse(completelyhomog2, NA, attr(vfl2o, 'SSErr')),
       ifelse(locallyhomog3[[lakei]], NA, attr(vfl3[[lakei]], 'SSErr')),
       ifelse(locallyhomog3[[lakei]], NA, attr(vfl3o[[lakei]], 'SSErr')),
+      ## ifelse(completelyhomog2, NA, attr(vfe2, 'SSErr')),
+      ## ifelse(completelyhomog2, NA, attr(vfe2o, 'SSErr')),
       ifelse(locallyhomog3[[lakei]], NA, attr(vfe3[[lakei]], 'SSErr')),
       ifelse(locallyhomog3[[lakei]], NA, attr(vfe3o[[lakei]], 'SSErr')),
+      ## ifelse(completelyhomog2, NA,
+      ##        sqrt(attr(vfl2, 'SSErr') / nrow(vfl2)) / maxgamma2), 
+      ## ifelse(completelyhomog2, NA,
+      ##        sqrt(attr(vfl2o, 'SSErr') / nrow(vfl2o)) / maxgamma2o),
       ifelse(locallyhomog3[[lakei]], NA,
              sqrt(attr(vfl3[[lakei]], 'SSErr') /
                   nrow(vfl3[[lakei]])) / maxgamma3[[lakei]]),
       ifelse(locallyhomog3[[lakei]], NA,
              sqrt(attr(vfl3o[[lakei]], 'SSErr') /
                   nrow(vfl3o[[lakei]])) / maxgamma3o[[lakei]]),
+      ## ifelse(completelyhomog2, NA,
+      ##        sqrt(attr(vfe2, 'SSErr') / nrow(vfe2)) / maxgamma2),
+      ## ifelse(completelyhomog2, NA,
+      ##        sqrt(attr(vfe2o, 'SSErr') / nrow(vfe2o)) / maxgamma2o),
       ifelse(locallyhomog3[[lakei]], NA,
              sqrt(attr(vfe3[[lakei]], 'SSErr') /
                   nrow(vfe3[[lakei]])) / maxgamma3[[lakei]]),
       ifelse(locallyhomog3[[lakei]], NA,
              sqrt(attr(vfe3o[[lakei]], 'SSErr') /
                   nrow(vfe3o[[lakei]])) / maxgamma3o[[lakei]]),
+      ## i2an.var[lakei],
+      ## i2ao.var[lakei],
+      ## i2bn.var[lakei],
+      ## i2bo.var[lakei],
+      ## i2cn.var[lakei],
+      ## i2co.var[lakei],
       i3an.var[lakei],
       i3ao.var[lakei],
       i3bn.var[lakei],
@@ -602,14 +773,34 @@ for (lakei in 1:nlakes) {
       i3co.var[lakei])
   cat(metadata, file = metafn)
   cat('\n', file = metafn, append = TRUE)
+  ## save(list = c('v2', 'v2o', 'v3', 'v3o'), file = variofn)
   v3l <- v3[[lakei]]
   v3ol <- v3[[lakei]]
   save(list = c('v3l', 'v3ol'), file = variofn)
 }
 cat(format(Sys.time(), "%a %b %d %X %Y"))
-cat('-- writing output to file done, end of loop\n')
+cat('\n')
 
-
+## for (lakei in 1:nlakes) {
+##   dimnames(interpolated[[lakei]])[[2]] <-
+##     c('i1a', 'i1b', 'i1c',
+##       'i2an', 'i2ao', 'i2bn', 'i2bo', 'i2cn', 'i2co',
+##       'i3an', 'i3ao', 'i3bn', 'i3bo', 'i3cn', 'i3co')
+##   dimnames(metadata[[lakei]])[[2]] <-
+##     c('panhomog', 'localhomog',
+##       paste0('maxgamma', c('2', '2o', '3', '3o')), 
+##       paste0('maxdist', c('2', '2o', '3', '3o')), 
+##       paste0('vfl', c('2', '2o', '3', '3o'), '.Lin'),
+##       paste0('vfe', rep(c('2', '2o', '3', '3o'), each = 6), '.',
+##              rep(c('Nug', 'Lin', 'Gau', 'Exp', 'Sph75', 'Sph50'), times = 4)),
+##       paste0('vfl', c('2', '2o', '3', '3o'), '.SSE'), 
+##       paste0('vfe', c('2', '2o', '3', '3o'), '.SSE'), 
+##       paste0('vfl', c('2', '2o', '3', '3o'), '.PartialRMSE'),
+##       paste0('vfe', c('2', '2o', '3', '3o'), '.PartialRMSE'),
+##       paste0('i', rep(c(2, 3), each = 6),
+##              rep(rep(c('a', 'b', 'c'), each = 2), times = 2),
+##              rep(c('n', 'o'), times = 6), '.var')) 
+  
 }
 
   
