8c692344c596fe2b6456f7c7d3dc5d540676c626
kojitominaga <koji.tominaga@ibv.uio.no>
2014-03-02 13:06:52 +0800

    a few enhancements and __main__ scripts done

--- nora10/nora10ck/abel/nora10util.py
+++ nora10/nora10ck/abel/nora10util.py
@@ -5,6 +5,7 @@ utilities for the interpolated data from NORA10
 import os
 import sys
 import tarfile
+import gzip
 
 varH = {'ta_2m':  '1H', 
         'ts_0m':  '1H', 
@@ -21,7 +22,7 @@ interpnames = ('i1a', 'i1b', 'i1c',
                'i3an', 'i3ao', 
                'i3bn', 'i3bo', 
                'i3cn', 'i3co')
-statsnames = ('locallyhomog3', 
+statnames = ('locallyhomog3', 
               'maxgamma3', 
               'maxgamma3o',
               'maxdist3', 
@@ -55,7 +56,7 @@ statsnames = ('locallyhomog3',
               'var3cn', 
               'var3co')
 
-def bylocation(location, var, timestat, years):
+def bylocation(location, var, timestat, years, outdir = '.'):
     '''
     all interpolation methods for a location
     writes a table with
@@ -78,12 +79,15 @@ def bylocation(location, var, timestat, years):
     if not all(p_exists):
         sys.exit('not all years of simulations are finished')
         
-    outfname = 'NORA10_%s_11km_%s_%s_%s_%s_%s' % (
+    outfname = 'NORA10_%s_11km_%s_%s_%s_%s_%s.gz' % (
         varH[var], var, '%s-%s' % (min(years), max(years)), timestat, location,
-        'interpolated_cutoff_100_nlocal_50.txt')
+        'interpolated_cutoff_100_nlocal_50.txt') 
+    outpath = os.path.join(outdir, outfname)
     
-    with open(outfname, 'w') as g:
+    with gzip.GzipFile(outpath, 'wb') as g:
         g.write(' '.join(interpnames))
+        g.write(' ')  
+        g.write(' '.join(statnames))      
         g.write('\n')
         for yi in range(len(pp)):
             p = pp[yi]
@@ -95,16 +99,24 @@ def bylocation(location, var, timestat, years):
                 i1, i2 = [int(i) for i in os.path.splitext(tarf)[0].split('-')]
                 with tarfile.open(os.path.join(p, tarf)) as tf:
                     for j in range(i1, i2 + 1):
-                        membername = 'NORA10_%s_11km_%s_%s_%04i_%s_%s' % (
+                        membername1 = 'NORA10_%s_11km_%s_%s_%04i_%s_%s' % (
                             varH[var], var, year, j, location, 
                             'interpolated_cutoff_100_nlocal_50.txt')
-                        memberf = tf.extractfile(membername)
-                        g.write(memberf.read().strip())
+                        memberf1 = tf.extractfile(membername1)
+                        g.write(memberf1.read().strip())
+                        memberf1.close()
+                        g.write(' ')
+                        membername2 = 'NORA10_%s_11km_%s_%s_%04i_%s_%s' % (
+                            varH[var], var, year, j, location, 
+                            'metadatainterp_cutoff_100_nlocal_50.txt')
+                        memberf2 = tf.extractfile(membername2)
+                        g.write(memberf2.read().strip())
+                        memberf2.close()
                         g.write('\n')
-                        memberf.close()
                     
 def byinterpmethodLocations(var, timestat, years, 
-                            interpmethod = 'last', locations = 'all'):
+                            interpmethod = 'last', locations = 'all',
+                            outdir = '.'):
     '''
     for the specified locations, and for the specified interp method, 
     write a table with
@@ -126,11 +138,12 @@ def byinterpmethodLocations(var, timestat, years,
                  if os.path.isdir(os.path.join('interpolated', d))] \
       if locations == 'all' else locations
     
-    outfname = 'NORA10_%s_11km_%s_%s_%s_interpolated_%s.txt' % (
+    outfname = 'NORA10_%s_11km_%s_%s_%s_interpolated_%s.txt.gz' % (
         varH[var], var, '%s-%s' % (min(years), max(years)), timestat,
         interpnames[interpmethod])
+    outpath = os.path.join(outdir, outfname)
 
-    with open(outfname, 'w') as g:
+    with gzip.GzipFile(outpath, 'wb') as g:
         g.write(' '.join(locations))
         g.write('\n')
         for year in years:
@@ -164,7 +177,8 @@ def byinterpmethodLocations(var, timestat, years,
             
                 
 def byinterpmethodVariables(location, timestat, H, years,
-                            interpmethod = 'last', varnames = 'all'):
+                            interpmethod = 'last', varnames = 'all',
+                            outdir = '.'):
     '''
     for a specified location, and for the specified interp method, 
     write a table with
@@ -186,11 +200,13 @@ def byinterpmethodVariables(location, timestat, H, years,
                 if os.path.isdir(os.path.join('interpolated', location, d))] \
                  if varnames == 'all' else varnames
     varnames = [var for var in varnames if varH[var] == H]
-    outfname = 'NORA10_%s_11km_%s_%s_%s_interpolated_%s.txt' % (
-        H, location, '%s-%s' % (min(years), max(years)), timestat,
-        interpnames[interpmethod])
+    outfname = 'NORA10_%s_11km_%s_%s_%s_%s_interpolated_%s.txt.gz' % (
+        H, '-'.join(varnames), location, 
+        '%s-%s' % (min(years), max(years)), timestat,
+        interpnames[interpmethod]) 
+    outpath = os.path.join(outdir, outfname)
 
-    with open(outfname, 'w') as g:
+    with gzip.GzipFile(outpath, 'w') as g:
         g.write(' '.join(varnames))
         g.write('\n')
         for year in years:
@@ -223,3 +239,83 @@ def byinterpmethodVariables(location, timestat, H, years,
                 g.write(' '.join([outdict[var][j] for var in varnames]))
                 g.write('\n')
             
+if __name__ == '__main__':
+    d1 = 'collated/by var by loc all interps and stats'
+    if not os.path.exists(d1): os.makedirs(d1)
+    d2 = 'collated/by var all locs by interp'
+    if not os.path.exists(d2): os.makedirs(d2)
+    d3 = 'collated/all vars by loc by interp'   
+    if not os.path.exists(d3): os.makedirs(d3)
+    years = range(2009, 2013)
+    takedailymean, timestat = (True, None)
+    ## if false define timestat here 
+    ## takedailymean, timestat == (False, 'all')
+    locations = [d for d in os.listdir('interpolated') 
+                 if os.path.isdir(os.path.join('interpolated', d))]
+    ## d1 all years exist
+    for location in locations:
+        for (var, H) in varH.items():
+            timestat = {'1H': 'mean24', '3H': 'mean8'}.setdefault(H, timestat)
+            paths = [os.path.join('interpolated', location, var, timestat,
+                     str(year), 'COMPLETED') for year in years]
+            allpathsexist = all([os.path.exists(path) for path in paths])
+            if allpathsexist:
+                expectedoutfn = \
+                  os.path.join(d1, 'NORA10_%s_11km_%s_%s_%s_%s_%s.gz' % (
+                      varH[var], var, '%s-%s' % (min(years), max(years)), 
+                      timestat, location,
+                      'interpolated_cutoff_100_nlocal_50.txt'))
+                if not os.path.exists(expectedoutfn):
+                    bylocation(location, var, timestat, years, outpath = d1)
+    ## d2 all years exist
+    for interpmethod in range(len(interpnames)):
+        for (var, H) in varH.items():
+            timestat = {'1H': 'mean24', '3H': 'mean8'}.setdefault(H, timestat)
+            paths = [os.path.join('interpolated', location, var, timestat,
+                     str(year), 'COMPLETED')
+                     for location in locations
+                     for year in years]
+            allpathsexist = all([os.path.exists(path) for path in paths])
+            if allpathsexist:
+                expectedoutfn = \
+                  os.path.join(d2, 
+                               'NORA10_%s_11km_%s_%s_%s_interpolated_%s.txt.gz'\
+                               % (varH[var], var, 
+                                  '%s-%s' % (min(years), max(years)), timestat,
+                                  interpnames[interpmethod]))
+                if not os.path.exists(expectedoutfn):
+                    byinterpmethodLocations(var, timestat, years, 
+                                            interpmethod = interpmethod, 
+                                            outdir = d2)
+    ## d3 all years exist
+    timestatdict = {'1H': 'mean24', '3H': 'mean8'}
+    for interpmethod in range(len(interpnames)):
+        for location in locations:
+            for (H, timestat) in timestatdict.items():
+                varnames = 'all'
+                varnames = \
+                  [d for d in 
+                   os.listdir(os.path.join('interpolated', location)) 
+                   if os.path.isdir(os.path.join('interpolated', 
+                                                 location, d))] \
+                   if varnames == 'all' else varnames
+                varnames = [var for var in varnames if varH[var] == H]
+                paths = [os.path.join('interpolated', location, var, timestat, 
+                         str(year), 'COMPLETED') 
+                         for var in varnames
+                         for year in years]
+                allpathsexist = all([os.path.exists(path) for path in paths])
+                if allpathsexist:
+                    expectedoutfn = \
+                      os.path.join(d3,
+                        'NORA10_%s_11km_%s_%s_%s_%s_interpolated_%s.txt.gz' % (
+                            H, '-'.join(varnames), location, 
+                            '%s-%s' % (min(years), max(years)), timestat,
+                            interpnames[interpmethod]))
+                    if not os.path.exists(expectedoutfn):
+                        byinterpmethodVariables(location, timestat, H, years, 
+                                                interpmethod = interpmethod,
+                                                outdir = d3)
+
+
+
