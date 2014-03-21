aa3edcf9aab59165f57e8327ff1d4c9dd4a96c61
kojitominaga <koji.tominaga@ibv.uio.no>
2014-03-21 12:38:09 +0100

    aux jobs

---
+++ nora10/nora10ck/abel/splitlocationsNIVA.py
@@ -0,0 +1,27 @@
+import os.path
+
+neach = 1
+
+f = open('locationsNIVA.csv')
+header = f.readline()
+lines = f.readlines()
+f.close()
+
+nlines = len(lines)
+nsplits = nlines / neach if nlines % neach == 0 else nlines // neach + 1
+
+if not os.path.exists('locations'): os.makedirs('locations')
+
+fnames = [os.path.join('locations', 'loc%03i.csv' % i) for i in range(nsplits)]
+starts = range(0, nlines, neach)
+ends = starts[1:] + [nlines]
+
+outs = [''.join([header] + lines[starts[i]:ends[i]]) for i in range(nsplits)] 
+## lines already include \n at the end
+
+for i in range(nsplits):
+    f = open(fnames[i], 'w')
+    f.write(outs[i])
+    f.close()
+
+
