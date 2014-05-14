a4acc30e38f46fc26dc3ec40609c3cf5dbf5edef
kojitominaga <koji.tominaga@ibv.uio.no>
2014-05-14 16:15:10 +0200

    back-porting cordex interp scripts to nora10

---
+++ nora10/nora10abel.py
@@ -0,0 +1,231 @@
+'''usage: python cordexabel.py ncpath lname llon llat prefix n onlygeog
+
+with default values for prefix = . and n = 500.
+'''
+import sys
+import os
+import numpy as np
+import netCDF4
+import datetime
+import time
+import pandas as pd
+
+sys.path.append('/cluster/home/kojito/lib/python/site-packages')
+
+import geopy.distance
+
+myfmt = '%.8f' ## 8 significant digits, e.g., 53.80798340
+nstats = 42
+
+# with open('RCMModelName.txt') as f:
+#     RCMlist = [l.rstrip().split()[:2] for l in f.readlines() if not l[0] == '#']
+#     RCMdict = dict(RCMlist)
+
+# def parsefn(fn, prefix = '.'):
+#     '''parses the cordex file name and returns 
+#     (the directory directory path, VariableName, StartTime, EndTime)
+#     StartTime and EndTime are None if Frequency is not day'''
+    
+#     assert fn[-3:] == '.nc', 'the file name has to have .nc'
+#     elements = fn.strip()[:-3].split('_')
+#     VariableName = elements[0]
+#     Domain = elements[1]
+#     GCMModelName = elements[2]
+#     CMIP5ExperimentName = elements[3]
+#     CMIP5EnsembleMember = elements[4]
+#     RCMModelName = elements[5]
+#     try:
+#         Institute = RCMdict[RCMModelName]
+#     except KeyError:
+#         sys.exit('RCM Model name not found')
+#     RCMVersionID = elements[6]
+#     Frequency = elements[7]
+#     StartTime = None
+#     EndTime = None
+#     if Frequency == 'day':
+#         StartTime, EndTime = elements[8].split('-')
+#     dirpath = os.path.join(prefix, Domain, Institute, GCMModelName, 
+#                            CMIP5ExperimentName, CMIP5EnsembleMember, 
+#                            RCMModelName, RCMVersionID, Frequency, VariableName)
+#     return (dirpath, VariableName, StartTime, EndTime)
+
+
+# def getorogfn(fn):
+#     '''returns the corresponding orography .nc file name'''
+    
+#     assert fn[-3:] == '.nc', 'the file name has to have .nc'
+#     elements = fn.strip()[:-3].split('_')
+#     # VariableName = 'orog'
+#     # Domain = elements[1]
+#     # GCMModelName = elements[2]
+#     # CMIP5ExperimentName = elements[3]
+#     # CMIP5EnsembleMember = elements[4]
+#     # RCMModelName = elements[5]
+#     # RCMVersionID = elements[6]
+#     # Frequency = 'fx'
+#     orogfn = '_'.join(['orog'] + elements[1:7] + ['fx']) + '.nc'
+#     return orogfn
+
+
+def writegeog(orogfn, dirname, lname, llat, llon, n, dummy = False):
+    '''extracts geographical information (longitude, latitude, altitude)
+    from orography .nc file. 
+    
+    Writes 3 files in dirname. "near-LOCATION-altitude.txt.gz" etc. 
+    
+    It also returns tuple of 4 elements:
+    
+    1) the boolean ndarray isnearby, the nearest n vertices being True
+    2) latpath
+    3) lonpath
+    4) altpath
+    '''
+    
+    latfn = 'near-%s_latitude.txt.gz' % lname
+    latpath = os.path.join(dirname, latfn)
+    lonfn = 'near-%s_longitude.txt.gz' % lname
+    lonpath = os.path.join(dirname, lonfn)
+    altfn = 'near-%s_altitude.txt.gz' % lname
+    altpath = os.path.join(dirname, altfn)
+    # distfn = 'near_%s-distance.txt.gz' % lname
+    # distpath = os.path.join(dirname, distfn)
+    r = netCDF4.Dataset(orogfn)
+    lon = r.variables['lon'][:, :].flatten()
+    lat = r.variables['lat'][:, :].flatten()
+    dist = np.array([geopy.distance.great_circle((llat, llon), (la, lo)).km
+                     for lo, la in zip(lon.tolist(), lat.tolist())])
+    
+    ## get the rank
+    temp = dist.argsort()
+    ranks = np.empty(len(dist), int)
+    ranks[temp] = np.arange(len(dist)) # smaller number closer to the loc
+    isnearby = ranks < n
+    
+    if not os.path.exists(dirname): os.makedirs(dirname)
+    if not dummy:
+        orog = r.variables['orog'][:, :].flatten()
+        print('[cordexabel.writegeog()] writing to %s' % lonpath)
+        np.savetxt(lonpath, lon[isnearby], fmt = myfmt)
+        print('[cordexabel.writegeog()] writing to %s' % latpath)
+        np.savetxt(latpath, lat[isnearby], fmt = myfmt)
+        print('[cordexabel.writegeog()] writing to %s' % altpath)
+        np.savetxt(altpath, orog[isnearby], fmt = '%.2f')
+        # np.savetxt(distpath, dist[isnearby], fmt = myfmt) 
+
+    r.close()
+    return (isnearby, latpath, lonpath, altpath)
+
+
+def spinterp(ncpath, orogpath, lname, llat, llon, lalt, 
+             prefix = '.', n = 500):
+    '''runs the spatial interpolation Rscript for all data along time unit
+    in ncfn, using the geography files (see writegeog()) in the directory
+    parsefn(getorogfn(ncfn), '.')[0], 
+    with the prediction at a new location with longitude (llon), 
+    latitude (llat) and altitude (lalt), and write outputs in 
+    os.path.join(parsefn(ncfn, '.')[0], lname). 
+
+    llat, llon, lalt: float
+
+    It will create necessary directories.'''
+
+    ncdir, ncfn = os.path.split(ncpath)
+
+    # orogfn = getorogfn(ncfn)
+    # orogpath = os.path.join(ncdir, orogfn)
+    # outdir, VariableName, StartTime, EndTime = parsefn(ncfn, prefix)
+
+    VariableName = '_'.join(ncfn.split('_')[3:-1])
+    
+    outdir = '.'
+
+    # if not os.path.exists(outdir): os.makedirs(outdir)
+    # print('[cordexabel.py] orogpath is %s' % orogpath)
+    # if not os.path.exists(orogpath):
+    #     print('[cordexabel.py] orogpath does not exist')
+    #     pref = '_'.join(orogfn.split('_')[:4])
+    #     suff = '_'.join(orogfn.split('_')[-3:])
+    #     filelist = os.listdir(ncdir)
+    #     alternatives = [f for f in filelist if pref in f and suff in f]
+    #     if len(alternatives) > 0:
+    #         print('[cordexabel.py] found %s instead' % alternatives[0])
+    #         orogfn = alternatives[0]
+    #         orogpath = os.path.join(ncdir, orogfn)
+    #     else: 
+    #         sys.exit('[cordexabel.py] ERROR: could not find the orog file')
+
+    geogdir = os.path.dirname(orogpath)
+    isnearby, latpath, lonpath, altpath = \
+      writegeog(orogpath, geogdir, lname, llat, llon, n, dummy = False)
+    # writes 3 .txt.gz files     
+    txtgzpath = os.path.join(outdir, 'near-%s_%s.txt.gz' % (
+        lname, os.path.splitext(ncfn)[0]))
+    r = netCDF4.Dataset(ncpath)
+    v = r.variables[VariableName]
+    t, y, x = v.shape
+    print('[cordexabel.py] writing variable file from the .nc file...')
+    
+    timeres = int(ncfn.split('_')[1][0])
+    new = v[:, :, :].reshape((t, y * x))[:, isnearby]
+    newdf = pd.DataFrame(new, index = np.repeat(np.arange(t / timeres), timeres))
+    dailymean = newdf.groupby(newdf.index).mean().values
+    
+    np.savetxt(txtgzpath, dailymean, fmt = myfmt)
+    # np.savetxt(txtgzpath, v[:, :, :].reshape((t, y * x))[:, isnearby], fmt = myfmt)
+
+    r.close()
+    resultspath = os.path.join(outdir, 'interpolated', lname, 
+                               'interpolated-%s_%s.txt.gz' % (
+                                   lname, os.path.splitext(ncfn)[0]))
+    resultsdir = os.path.dirname(resultspath)
+
+    do_interp_flag = True
+    
+    if not os.path.exists(resultsdir): os.makedirs(resultsdir)
+        
+    if do_interp_flag:
+        print('[cordexabel.py] interpolating -- calling R...')
+        ## usage: Rscript spinterp_cordex.R --args \
+        ##        lonpath latpath orogpath varpath llon llat lalt resultspath
+        command = '%s \\\n%s \\\n%s \\\n%s \\\n%s \\\n%s %s %s \\\n%s' % (
+          'Rscript spinterp_cordex.R --args', 
+          lonpath, latpath, altpath, txtgzpath, llon, llat, lalt, resultspath)
+        print(command)
+        os.system(command)
+    os.unlink(txtgzpath)
+
+if __name__ == '__main__':
+    a = sys.argv
+    ncpath = a[1]
+    lname = a[2]
+    llon = a[3]
+    llat = a[4]
+    lalt = a[5]
+    prefix = '.' if len(a) <= 6 else a[6]
+    n = 500 if len(a) <= 7 else int(a[7])
+    onlygeog = False if len(a) <= 8 else bool(a[8])
+    if onlygeog:
+        print('[cordexabel.py] creating geog files only')
+        ncdir, ncfn = os.path.split(ncpath)
+        orogfn = getorogfn(ncfn)
+        orogpath = os.path.join(ncdir, orogfn)
+        geogdir = parsefn(orogfn, prefix)[0]
+        outdir, VariableName, StartTime, EndTime = parsefn(ncfn, prefix)
+        if not os.path.exists(outdir): os.makedirs(outdir)
+        print('[cordexabel.py] orogpath is %s' % orogpath)
+        if not os.path.exists(orogpath):
+            print('[cordexabel.py] orogpath does not exist')
+            pref = '_'.join(orogfn.split('_')[:4])
+            suff = '_'.join(orogfn.split('_')[-3:])
+            filelist = os.listdir(ncdir)
+            alternatives = [f for f in filelist if pref in f and suff in f]
+            if len(alternatives) > 0:
+                print('[cordexabel.py] found %s instead' % alternatives[0])
+                orogfn = alternatives[0]
+                orogpath = os.path.join(ncdir, orogfn)
+            else: 
+                sys.exit('[cordexabel.py] ERROR: could not find the orog file')
+        isnearby, latpath, lonpath, altpath = \
+          writegeog(orogpath, geogdir, lname, llat, llon, n, dummy = False)
+    else:
+        spinterp(ncpath, lname, llat, llon, lalt, prefix, n)
---
+++ nora10/spinterp_cordex.R
@@ -0,0 +1,197 @@
+## usage: Rscript spinterp_cordex.R --args \
+##          lonpath latpath altpath varpath llon llat lalt resultspath
+
+dbglv <- 0 # debug.level
+
+nlocal <- 100 # number of points 
+cutoff <- 50 # max distance
+ninterpmethods <- 9
+nmetadata <- 33
+## boundariesMid <- sqrt(c(1, 2, 4, 5, 8, 9, 10, 13, 16, 17, 18)) * 12.245
+## ## at grid 0.11 degree
+## > boundariesMid
+##  [1] 12.24500 17.31705 24.49000 27.38065 34.63409 36.73500 38.72209 44.14998
+##  [9] 48.98000 50.48743 51.95114
+boundaries <- c(15, 20, 25.8, 30, 35.2, 37.7, 42, 46, 53, 62, 71, 80, 90, 101)
+
+cat('====\nThis is Rscript\n')
+cat(format(Sys.time(), "%a %b %d %X %Y"))
+cat('\ncurrent working directory is:\n')
+cat(getwd())
+cat('\n====\n')
+
+lonpath <- commandArgs(trailingOnly = TRUE)[2]
+latpath <- commandArgs(trailingOnly = TRUE)[3]
+altpath <- commandArgs(trailingOnly = TRUE)[4]
+varpath <- commandArgs(trailingOnly = TRUE)[5]
+llon <- as.numeric(commandArgs(trailingOnly = TRUE)[6])
+llat <- as.numeric(commandArgs(trailingOnly = TRUE)[7])
+lalt <- as.numeric(commandArgs(trailingOnly = TRUE)[8])
+resultspath <- commandArgs(trailingOnly = TRUE)[9]
+
+## lonpath <- 'EUR-11/DMI/ICHEC-EC-EARTH/rcp85/r3i1p1/DMI-HIRHAM5/v1/fx/orog/near-testlocation-longitude.txt.gz'
+## latpath <- 'EUR-11/DMI/ICHEC-EC-EARTH/rcp85/r3i1p1/DMI-HIRHAM5/v1/fx/orog/near-testlocation-latitude.txt.gz'
+## altpath <- 'EUR-11/DMI/ICHEC-EC-EARTH/rcp85/r3i1p1/DMI-HIRHAM5/v1/fx/orog/near-testlocation-altitude.txt.gz'
+## varpath <- 'EUR-11/DMI/ICHEC-EC-EARTH/rcp85/r3i1p1/DMI-HIRHAM5/v1/day/tas/near-testlocation-tas_EUR-11_ICHEC-EC-EARTH_rcp85_r3i1p1_DMI-HIRHAM5_v1_day_20910101-20951231.txt.gz'
+## outdir <- '.'
+## llon <- 10
+## llat <- 60
+## lalt <- 152
+
+cat(sprintf('[spinterp_cordex.R] lonpath is %s\n', lonpath))
+cat(sprintf('[spinterp_cordex.R] latpath is %s\n', latpath))
+cat(sprintf('[spinterp_cordex.R] altpath is %s\n', altpath))
+cat(sprintf('[spinterp_cordex.R] varpath is %s\n', varpath))
+cat(sprintf('[spinterp_cordex.R] llon is %s\n', llon))
+cat(sprintf('[spinterp_cordex.R] llat is %s\n', llat ))
+cat(sprintf('[spinterp_cordex.R] lalt is %s\n', lalt))
+cat(sprintf('[spinterp_cordex.R] resultspath is %s\n', resultspath))
+
+require(intervals)
+require(sp)
+require(gstat)
+## require(intervals, lib.loc = 'R')
+## require(sp, lib.loc = 'R')
+## require(gstat, lib.loc = 'R')
+
+llCRS <- CRS('+proj=longlat +ellps=WGS84 +datum=WGS84')
+
+lon <- scan(lonpath, quiet = TRUE)
+lat <- scan(latpath, quiet = TRUE)
+alt <- scan(altpath, quiet = TRUE)
+orogdf <- data.frame(lat, lon, orog = alt)
+orogdfgeo <- orogdf
+coordinates(orogdfgeo) <- c('lon', 'lat')
+proj4string(orogdfgeo) <- llCRS
+
+require(gstat)
+## require(gstat, lib.loc = 'R')
+
+newloc <- data.frame(lon = llon, lat = llat, orog = lalt)
+coordinates(newloc) <- c('lon', 'lat')
+proj4string(newloc) <- llCRS
+
+## read raw data
+raw <- as.matrix(read.table(varpath))
+n <- nrow(raw) ## number of time points
+
+results <- matrix(nrow = n, ncol = ninterpmethods + nmetadata)
+
+ranks <- rank(spDistsN1(orogdfgeo, newloc, longlat = TRUE)) ## rank distance
+
+for (i in 1:n) {
+  if ((i %% 100) == 0) cat(sprintf('[spinterp_cordex.R] %s of %s\n', i, n))
+  vi <- raw[i, ]
+  r <- data.frame(orogdf, v = vi)
+  coordinates(r) <- c('lon', 'lat')
+  proj4string(r) <- llCRS
+  ## MAIN PURPOSE: now use r to estimate v at newloc ## 
+
+  lh <- length(unique(r[['v']][ranks <= 5])) == 1 ## locally homogeneous
+  ## 1a: nearest value
+  i1a <- r[['v']][ranks == 1]
+  if (lh) {
+    i1b <- i1a ; i1c <- i1a
+    v3 <- NA ; v3o <- NA
+    mg <- NA ; mgo <- NA
+    md <- NA ; mdo <- NA
+    vfl3 <- NA ; vfl3o <- NA
+    vfe3 <- NA ; vfe3o <- NA
+    k3an <- NA ; i3an <- i1a ; i3an.var <- NA
+    k3ao <- NA ; i3ao <- i1a ; i3ao.var <- NA
+    k3bn <- NA ; i3bn <- i1a ; i3bn.var <- NA
+    k3bo <- NA ; i3bo <- i1a ; i3bo.var <- NA
+    k3cn <- NA ; i3cn <- i1a ; i3cn.var <- NA
+    k3co <- NA ; i3co <- i1a ; i3co.var <- NA
+  } else {
+    ## 1b: mean of nearest four
+    i1b <- mean(r[['v']][ranks <= 4])
+
+    ## 1c: triangular 2D linear interpolation
+    n1 <- r[ranks == 1, ] ; n2 <- r[ranks == 2, ] ; n3 <- r[ranks == 3, ]
+    cx <- coordinates(newloc)[1, 1] ; cy <- coordinates(newloc)[1, 2]
+    n1x <- coordinates(n1)[1, 1] ; n1y <- coordinates(n1)[1, 2]
+    n2x <- coordinates(n2)[1, 1] ; n2y <- coordinates(n2)[1, 2]
+    n3x <- coordinates(n3)[1, 1] ; n3y <- coordinates(n3)[1, 2]
+    ## now say,
+    ## p1|n2(x, y) - n1(x, y)| + p2|n3(x, y) - n1(x, y)| = c(x, y)
+    p2 <- ((cy - n1y) * (n2x - n1x) / (n2y - n1y) - (cx - n1x)) /
+      ((n3y - n1y) * (n2x - n1x) / (n2y - n1y) - (n3x - n1x))
+    p1 <- ((cx - n1x) - p2 * (n3x - n1x)) / (n2x - n1x)
+    ## p1a <- ((cy - n1y) - p2 * (n3y - n1y)) / (n2y - n1y)
+    ## p1a should be the same as p1
+    out <- n1[['v']] + p1 * (n2[['v']] - n1[['v']]) +
+      p2 * (n3[['v']] - n1[['v']])
+    attr(out, "names") <- NULL                
+    i1c <- out
+
+    ## variograms 
+    v3 <- variogram(v ~ 1, r, boundaries = boundaries)
+    v3o <- variogram(v ~ orog, r, boundaries = boundaries)
+    mg <- max(v3[['gamma']]) ; mgo <- max(v3o[['gamma']])
+    md <- max(v3[['dist']]) ; mdo <- max(v3o[['dist']])
+
+    ## variogram models 
+    vfl3 <- fit.variogram(v3, vgm(mg, 'Lin', md),
+                          fit.sills = TRUE, fit.ranges = FALSE)
+    vfl3o <- fit.variogram(v3o, vgm(mgo, 'Lin', mdo),
+                           fit.sills = TRUE, fit.ranges = FALSE)
+    vfe3 <- fit.variogram(v3,
+                          vgm(mg / 2, 'Sph', md * 0.25,
+                              add.to = vgm(mg / 2, 'Sph', md * 0.50,
+                                add.to = vgm(mg / 2, 'Exp', md,
+                                  add.to = vgm(mg / 2, 'Gau', md,
+                                    add.to = vgm(mg / 2, 'Lin', md,
+                                      mg / 2))))),
+                          fit.sills = TRUE, fit.ranges = FALSE)
+    vfe3o <- fit.variogram(v3o,
+                           vgm(mgo / 2, 'Sph', mdo * 0.25,
+                               add.to = vgm(mgo / 2, 'Sph', mdo * 0.50,
+                                 add.to = vgm(mgo / 2, 'Exp', mdo,
+                                   add.to = vgm(mgo / 2, 'Gau', mdo,
+                                     add.to = vgm(mgo / 2, 'Lin', mdo,
+                                       mgo / 2))))),
+                           fit.sills = TRUE, fit.ranges = FALSE)
+    
+    ## 3an* : inverse distance
+    k3an <- krige(v ~ 1, r, newloc, nmax = nlocal, debug.level = dbglv)
+    i3an <- k3an[['var1.pred']] ; i3an.var <- k3an[['var1.var']]
+    ## 3ao* : inverse distance with weighting by orog
+    k3ao <- krige(v ~ orog, r, newloc, nmax = nlocal, debug.level = dbglv)
+    i3ao <- k3ao[['var1.pred']] ; i3ao.var <- k3ao[['var1.var']]
+    ## 3bn* : kriging (linear variogram model), ordinary
+    k3bn <- krige(v ~ 1, r, newloc, vfl3, nmax = nlocal, debug.level = dbglv)
+    i3bn <- k3bn[['var1.pred']] ; i3bn.var <- k3bn[['var1.var']]
+    ## 3bo* : kriging (linear variogram model), universal
+    k3bo <- krige(v ~ orog, r, newloc, vfl3o, nmax = nlocal, debug.level = dbglv)
+    i3bo <- k3bo[['var1.pred']] ; i3bo.var <- k3bo[['var1.var']]
+    ## 3cn* : kriging (complex variogram model), ordinary
+    k3cn <- krige(v ~ 1, r, newloc, vfe3, nmax = nlocal, debug.level = dbglv)
+    i3cn <- k3cn[['var1.pred']] ; i3cn.var <-k3cn[['var1.var']]
+    ## 3co* : kriging (complex variogram model), universal
+    k3co <- krige(v ~ orog, r, newloc, vfe3o, nmax = nlocal, debug.level = dbglv)
+    i3co <- k3co[['var1.pred']] ; i3co.var <- k3co[['var1.var']]
+  }
+  
+  results[i, ] <-
+    c(i1a, i1b, i1c, i3an, i3ao, i3bn, i3bo, i3cn, i3co, lh * 1, mg, mgo, md, mdo,
+      round(ifelse(lh, NA, vfl3[['psill']]) / mg, digits = 3),
+      round(ifelse(lh, NA, vfl3o[['psill']]) / mgo, digits = 3),
+      round(ifelse(rep(lh, times = 6), rep(NA, times = 6), vfe3[['psill']]) / mg, digits = 3), ## 6 elements
+      round(ifelse(rep(lh, times = 6), rep(NA, times = 6), vfe3o[['psill']]) / mgo, digits = 3), ## 6 elements
+      ifelse(lh, NA, attr(vfl3, 'SSErr')), ifelse(lh, NA, attr(vfl3o, 'SSErr')),
+      ifelse(lh, NA, attr(vfe3, 'SSErr')), ifelse(lh, NA, attr(vfe3o, 'SSErr')),
+      ifelse(lh, NA, round(sqrt(attr(vfl3, 'SSErr') / nrow(vfl3)) / mg, digits = 3)),
+      ifelse(lh, NA, round(sqrt(attr(vfl3o, 'SSErr') / nrow(vfl3o)) / mgo, digits = 3)),
+      ifelse(lh, NA, round(sqrt(attr(vfe3, 'SSErr') / nrow(vfe3)) / mg, digits = 3)),
+      ifelse(lh, NA, round(sqrt(attr(vfe3o, 'SSErr') / nrow(vfe3o)) / mgo, digits = 3)),
+      i3an.var, i3ao.var, i3bn.var, i3bo.var, i3cn.var, i3co.var)
+}
+
+cat(sprintf('[spinterp_cordex.R] now writing the results to %s\n', resultspath))
+gzf <- gzfile(description = resultspath, open = 'w')
+write.table(results, file = gzf, row.names = FALSE, col.names = FALSE)
+close(gzf)
+  
+
+
