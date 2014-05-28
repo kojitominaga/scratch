7ddfb04b7f54ae5c856c045ecafea1b291d713f6
kojitominaga <koji.tominaga@ibv.uio.no>
2014-05-28 13:45:04 +0200

    hmmm still not sure if this is correct

--- nora10/reneworog.py
+++ nora10/reneworog.py
@@ -1,12 +1,16 @@
 import netCDF4
 
-new = netCDF4.Dataset('NORA10_11km_orog_new.nc', 'a')
+new = netCDF4.Dataset('NORA10_11km_orog_new3.nc', 'a')
 r = netCDF4.Dataset('NORA10_11km_LATLON.nc')
 
-new.variables['lat'][:, :] = r.variables['lat'][:, :]
-new.variables['lon'][:, :] = r.variables['lon'][:, :]
-new.variables['rlat'][:] = r.variables['rlat'][:]
-new.variables['rlon'][:] = r.variables['rlon'][:]
+rlat = r.variables['rlat'][:]
+new.variables['rlat'][:] = rlat
+# rlon = r.variables['rlon'][:]
+# new.variables['rlon'][:] = rlon
+lat = r.variables['lat'][:, :]
+new.variables['lat'][:, :] = lat
+lon = r.variables['lon'][:, :]
+new.variables['lon'][:, :] = lon
 
 new.close()
 r.close()
