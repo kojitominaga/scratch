d3c3f50aadc491580405a3d028268b587552c067
kojitominaga <koji.tominaga@ibv.uio.no>
2014-06-02 16:35:36 +0200

    more compact job names

--- nora10/metacreate.py
+++ nora10/metacreate.py
@@ -14,7 +14,7 @@ orog = '/work/users/kojito/nora10/nc/orog/NORA10_11km_orog_new3.nc'
 
 def customsubmit(varname, timeres, name, lat, lon, alt, initial = False):
     cm = cmda if initial else cmd
-    scriptfname = '%s_%s.sh' % (name, varname)
+    scriptfname = '%s_%s.sh' % ('C' + name[6:], varname)
     c = '%s %s %s %s %s %s/%s/NORA10_%s_11km_%s_ %s %s %s %s' % (
         cm, name, lon, lat, alt, ncdir, varname, timeres, varname, 
         start, end, orog, scriptfname)
@@ -23,13 +23,13 @@ def customsubmit(varname, timeres, name, lat, lon, alt, initial = False):
 
 for name, lat, lon, alt in g:
     # customsubmit('ta_2m', '1H', name, lat, lon, alt, initial=True)
-    customsubmit('pr', '1H', name, lat, lon, alt)
-    customsubmit('wss_10m', '1H', name, lat, lon, alt)
-    customsubmit('hur_2m', '1H', name, lat, lon, alt)
-    customsubmit('ps', '3H', name, lat, lon, alt)
-    customsubmit('clt', '1H', name, lat, lon, alt)
+    # customsubmit('pr', '1H', name, lat, lon, alt)
+    # customsubmit('wss_10m', '1H', name, lat, lon, alt)
+    # customsubmit('hur_2m', '1H', name, lat, lon, alt)
+    # customsubmit('ps', '3H', name, lat, lon, alt)
+    # customsubmit('clt', '1H', name, lat, lon, alt)
     # customsubmit('albedo', '1H', name, lat, lon, alt)
-    # customsubmit('rls', '1H', name, lat, lon, alt)
+    customsubmit('rls', '1H', name, lat, lon, alt)
     # customsubmit('rss', '1H', name, lat, lon, alt)
 
 
