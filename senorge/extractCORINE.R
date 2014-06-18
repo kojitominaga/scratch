3fe7b989706ec95fb1f45d4f41b4a9d5b7611dc6
kojitominaga <koji.tominaga@ibv.uio.no>
2014-06-18 18:38:05 +0200

    corine gIntersections

---
+++ senorge/extractCORINE.R
@@ -0,0 +1,96 @@
+## Rscript extractSeNorge.R --args year
+
+require(raster)
+require(rgdal)
+require(RSAGA)
+require(sp)
+require(rgeos)
+
+myRAG <- function (file, return.header = TRUE, print = 0, nodata.values = c(), 
+    at.once = TRUE, na.strings = "NA") 
+{
+    if (is.character(file)) {
+        file = default.file.extension(file, ".asc")
+        con = file(file, open = "r")
+        on.exit(close(con), add = TRUE)
+    }
+    else {
+        con = file
+        if (!isOpen(file, "read")) 
+            stop("'file' must be a file name or a connection opened for reading")
+    }
+    hdr = read.ascii.grid.header(con)
+    if (at.once) {
+        data = scan(con, quiet = TRUE, na.strings = na.strings)
+        ## data = scan(con, nlines = hdr$nrows, quiet = TRUE, na.strings = na.strings)
+        data = matrix(data, ncol = hdr$ncols, nrow = hdr$nrows, 
+            byrow = TRUE)
+        na = is.na(data) | (data == hdr$nodata_value)
+        for (na.val in nodata.values) na = na | (data == na.val)
+        if (any(na)) 
+            data[na] = NA
+    }
+    else {
+        data = matrix(NA, ncol = hdr$ncols, nrow = hdr$nrows)
+        for (i in 1:hdr$nrows) {
+            if (print == 2) 
+                cat(i, " ", ifelse(round(i/20) == i/20, "\n", 
+                  ""))
+            if (print == 1) 
+                if (round(i/100) == i/100) 
+                  cat(i, " ", ifelse(round(i/1000) == i/1000, 
+                    "\n", ""))
+            x = scan(con, nlines = 1, quiet = TRUE, na.strings = na.strings)
+            na = is.na(x) | (x == hdr$nodata_value)
+            for (na.val in nodata.values) na = na | (x == na.val)
+            if (any(na)) 
+                x[na] = NA
+            data[i, ] = x
+        }
+    }
+    if (print == 2) 
+        cat("\nDone!\n")
+    if (print == 1) 
+        cat("\n")
+    if (return.header) 
+        data = list(header = hdr, data = data)
+    invisible(data)
+}
+
+
+
+
+year <- as.integer(commandArgs(trailingOnly = TRUE)[1])
+cat('year is ') ; cat(sprintf('%s', year)) ; cat('\n')
+
+
+dir <- sprintf('/work/users/kojito/senorge/gwb_ascii_%s', year)
+dates <- seq(from=as.Date(sprintf('%s-01-01', year)),
+             to=as.Date(ifelse(year == 2014,
+               sprintf('%s-06-10', year),
+               sprintf('%s-12-31', year))),
+             by='day')
+
+nbf.sub <- readOGR('catchments', 'nbf.sub')
+nbf.sub.data <- slot(nbf.sub, 'data')
+
+corina <- readOGR('CORINE2006-20140618-79dc87',
+                  'CORINE2006_Norge_79dc87_411_412')
+corina <- spTransform(corina, CRS(proj4string(nbf.sub)))
+
+bogs <- numeric(length = nrow(nbf.sub))
+for (i in 1:nrow(nbf.sub)) {
+  cat(i) ; cat('\n')
+  intersection <- gIntersection(nbf.sub[i, ], corina)
+  if (is.null(intersection)) {
+    bogs[i] <- 0
+  } else {
+    bogs[i] <- gArea(intersection) / gArea(nbf.sub[i, ])
+  }
+}
+df <- nbf.sub@data
+df[['bogs']] <- bogs
+write.csv(df, file = 'bogs.csv')
+
+
+    
