3f6e68a1a564f9cfcc64de5c961f1528f2d97774
kojitominaga <koji.tominaga@ibv.uio.no>
2014-08-14 18:24:13 +0200

    fetch

--- comsat/GIS/fetch.R
+++ comsat/GIS/fetch.R
@@ -7,13 +7,13 @@ geographical <- CRS('+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs')
 utm33n <- CRS('+proj=utm +zone=33 +ellps=WGS84 +datum=WGS84 +units=m +no_defs')
 comsat <- spTransform(comsatgeo, utm33n)
 
-if (!file.exists('buffer_demo')) dir.create('buffer_demo')
+if (!file.exists('fetch')) dir.create('fetch')
 
 getfetch <- function(pol, angle, n=100) {  ## angle in degrees clockwise
   polr <- elide(pol, rotate=angle)
   proj4string(polr) <- utm33n
   bb <- bbox(polr)
-  xx <- as.list(seq(from=bb[1, 1], to=bb[1, 2], length.out=102)[2:101])
+  xx <- as.list(seq(from=bb[1, 1], to=bb[1, 2], length.out=n+2)[2:(n+1)])
   ll <- lapply(xx, function(x) {
     Lines(list(Line(matrix(c(x, bb[2, 1], x, bb[2, 2]),
                            byrow=TRUE, ncol=2, nrow=2))),
@@ -31,26 +31,60 @@ getfetch <- function(pol, angle, n=100) {  ## angle in degrees clockwise
   return(list(maxlength=maxlength, maxline=maxline))
 }
 
+ff <- data.frame(f0=NA, f10=NA, f20=NA, f30=NA, f40=NA, f50=NA, f60=NA,
+                 f70=NA, f80=NA, f90=NA, f100=NA, f110=NA, f120=NA, f130=NA,
+                 f140=NA, f150=NA, f160=NA, f170=NA)
 for (ri in 1:nrow(comsat)) {
   thislake <- comsat[ri, ]
   fetch0 <- getfetch(thislake, 0)
+  fetch10 <- getfetch(thislake, 350)
+  fetch20 <- getfetch(thislake, 340)
   fetch30 <- getfetch(thislake, 330)
+  fetch40 <- getfetch(thislake, 320)
+  fetch50 <- getfetch(thislake, 310)
   fetch60 <- getfetch(thislake, 300)
+  fetch70 <- getfetch(thislake, 290)
+  fetch80 <- getfetch(thislake, 280)
   fetch90 <- getfetch(thislake, 270)
+  fetch100 <- getfetch(thislake, 260)
+  fetch110 <- getfetch(thislake, 250)
   fetch120 <- getfetch(thislake, 240)
+  fetch130 <- getfetch(thislake, 230)
+  fetch140 <- getfetch(thislake, 220)
   fetch150 <- getfetch(thislake, 210)
+  fetch160 <- getfetch(thislake, 200)
+  fetch170 <- getfetch(thislake, 190)
   thislake@data <- data.frame(fetch0=round(fetch0[['maxlength']]),
+                              fetch10=round(fetch10[['maxlength']]),
+                              fetch20=round(fetch20[['maxlength']]),
                               fetch30=round(fetch30[['maxlength']]),
+                              fetch40=round(fetch40[['maxlength']]),
+                              fetch50=round(fetch50[['maxlength']]),
                               fetch60=round(fetch60[['maxlength']]),
+                              fetch70=round(fetch70[['maxlength']]),
+                              fetch80=round(fetch80[['maxlength']]),
                               fetch90=round(fetch90[['maxlength']]),
+                              fetch100=round(fetch100[['maxlength']]),
+                              fetch110=round(fetch110[['maxlength']]),
                               fetch120=round(fetch120[['maxlength']]),
+                              fetch130=round(fetch130[['maxlength']]),
+                              fetch140=round(fetch140[['maxlength']]),
                               fetch150=round(fetch150[['maxlength']]),
+                              fetch160=round(fetch160[['maxlength']]),
+                              fetch170=round(fetch170[['maxlength']]),
                               thislake@data)
   name <- thislake@data[['comsat.Lake']]
-  writeOGR(spTransform(thislake, geographical),
-           sprintf('fetch/%s.geojson', name),
-           'comsat', 'GeoJSON')
+  fn <- sprintf('fetch/%s.geojson', name)
+  if (!file.exists(fn)) {
+    writeOGR(spTransform(thislake, geographical), fn, 'comsat', 'GeoJSON')
+  }
+  ff[ri, ] <- thislake@data[1, 1:18]
 }
+
+comsat@data <- data.frame(comsat@data, ff)
+writeOGR(spTransform(comsat, geographical), 'comsat_fetch.geojson',
+         'comsat', 'GeoJSON')
+
 
   ## spl <- rbind(SpatialLines(list(fetch0[['maxline']])),
   ##                elide(SpatialLines(list(fetch30[['maxline']])),
