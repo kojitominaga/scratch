065f36b399c3a48a4460f38ae929bb31af779959
kojitominaga <koji.tominaga@ibv.uio.no>
2015-02-12 14:49:08 +0100

    full working version per lake

--- lakes/mylake.py
+++ lakes/mylake.py
@@ -4,6 +4,10 @@ import h5py
 import datetime
 import os
 import math
+import sys
+
+depth_resolution = 0.1 # metre
+
 
 def mylakeinit(max_depth, area, outpath):
     '''max_depth: m, area: m2.
@@ -21,8 +25,9 @@ def mylakeinit(max_depth, area, outpath):
     firstlines = '''-999	"MyLake init"
 Z (m)	Az (m2)	Tz (deg C)	Cz	Sz (kg/m3)	TPz (mg/m3)	DOPz (mg/m3)	Chlaz (mg/m3)	DOCz (mg/m3)	TPz_sed (mg/m3)	Chlaz_sed (mg/m3)	"Fvol_IM (m3/m3	 dry w.)"	Hice (m)	Hsnow (m)'''
     lines = [firstlines] + lines
-    with open('init', 'w') as f:
+    with open(outpath, 'w') as f:
         f.write('\n'.join(lines))
+
 
 def mylakepar(longitude, latitude, outpath, atten_coeff=1):
     '''atten_coeff: m-1 
@@ -81,11 +86,13 @@ qy_DOC	0.1	NaN	NaN	mg mol-1
     with open(outpath, 'w') as f:
         f.write(out)
 
-def take5(pdict, dates, e):
+
+def take5(pdict, dates, eh):
     '''pdict: dictionary of paths to HDF5 files (see pA and pB)
     dates: see datesA and datesB
-    e: ebhex
+    eh: ebhex
     returns pandas.DataFrame'''
+    e = eh.strip('0x').strip('0')
     df = pd.DataFrame(dates, columns = ['date'])
     df['clt'] = h5py.File(pdict['clt'], mode='r')[e][:] * 0.01
     df['hurs'] = h5py.File(pdict['hurs'], mode='r')[e][:]
@@ -95,6 +102,7 @@ def take5(pdict, dates, e):
     df['sfcWind'] = h5py.File(pdict['sfcWind'], mode='r')[e][:]
     df['tas'] = h5py.File(pdict['tas'], mode='r')[e][:] - 273.15
     return df
+
 
 def mylakeinput(pA, pB, datesA, datesB, eh, outpath):
     df = pd.concat([take5(pA, datesA, eh), take5(pB, datesB, eh)])
@@ -132,41 +140,83 @@ def mylakeinput(pA, pB, datesA, datesB, eh, outpath):
             g.write(f.read().replace('-99999999', 'NaN'))
     os.unlink(temporarypath)        
 
-if __name__ == '__main__':
-    model = 'EUR-11_ICHEC-EC-EARTH_historical_r1i1p1_KNMI-RACMO22E_v1_day'
-    y1 = 1971
-    latitude = 59.97
-    longitude = 10.62
-    eh = '1cb62'
-    # eh = 'b516a3' ## a lake in the north
-    area = 1100000
-    depth = max(4.0, math.sqrt(area) / 100)
-    y2 = y1 + 4
-    y3 = y1 + 5
-    y4 = y1 + 9
-    
-    variables = ['clt', 'hurs', 'tas', 'rsds', 'ps', 'pr', 'sfcWind']
 
-    prefix = '/work/users/kojito/cordex'
-    pA = {v: '%s/Lakes_%s_%s_%s0101-%s1231.h5' % (prefix, v, model, y1, y2)
-          for v in variables}
-    pB = {v: '%s/Lakes_%s_%s_%s0101-%s1231.h5' % (prefix, v, model, y3, y4)
-          for v in variables}
-    
-    ndaysAB = (datetime.date(y4, 12, 31) - datetime.date(y1, 1, 1)).days + 1
-    datesAB = [datetime.date(y1, 1, 1) + datetime.timedelta(d)
-               for d in range(ndaysAB)]
-    ndaysA = (datetime.date(y2, 12, 31) - datetime.date(y1, 1, 1)).days + 1
-    datesA = [datetime.date(y1, 1, 1) + datetime.timedelta(d) 
+variables = ['clt', 'hurs', 'tas', 'rsds', 'ps', 'pr', 'sfcWind']
+models = {1: ('ICHEC-EC-EARTH', 'r1i1p1_KNMI-RACMO22E_v1_day'), 
+          2: ('ICHEC-EC-EARTH', 'r3i1p1_DMI-HIRHAM5_v1_day_'), 
+          3: ('MPI-M-MPI-ESM-LR', 'r1i1p1_CLMcom-CCLM4-8-17_v1_day_')}
+scenarios = {1: ('historical', 1971, 'historical', 1976), 
+             2: ('historical', 2001, 'rcp45', 2006), 
+             3: ('rcp45', 2031, 'rcp45', 2036), 
+             4: ('rcp45', 2061, 'rcp45', 2066), 
+             5: ('rcp45', 2091, 'rcp45', 2096), 
+             6: ('rcp85', 2031, 'rcp85', 2036), 
+             7: ('rcp85', 2061, 'rcp85', 2066), 
+             8: ('rcp85', 2091, 'rcp85', 2096)}
+prefix = '/work/users/kojito/cordex' # prefix
+prefix2 = '/work/users/kojito/lakes'
+def runlake(modelid, scenarioid, eh, depth, area, longitude, latitude):
+    '''see above lines for models and scenarios (dictionaries). eh: EBhex'''
+    exA, y1A, exB, y1B = scenarios[scenarioid]
+    m1, m2 = models[modelid]
+    y2A = y1A + 4
+    y2B = y1B + 4
+    pA = {v: '%s/Lakes_%s_EUR-11_%s_%s_%s_%s0101-%s1231.h5' % 
+          (prefix, v, m1, exA, m2, y1A, y2A) for v in variables}
+    pB = {v: '%s/Lakes_%s_EUR-11_%s_%s_%s_%s0101-%s1231.h5' % 
+          (prefix, v, m1, exB, m2, y1B, y2B) for v in variables}
+    ndaysA = (datetime.date(y2A, 12, 31) - datetime.date(y1A, 1, 1)).days + 1
+    datesA = [datetime.date(y1A, 1, 1) + datetime.timedelta(d) 
               for d in range(ndaysA)]
-    ndaysB = (datetime.date(y4, 12, 31) - datetime.date(y3, 1, 1)).days + 1
-    datesB = [datetime.date(y3, 1, 1) + datetime.timedelta(d)
+    ndaysB = (datetime.date(y2B, 12, 31) - datetime.date(y1B, 1, 1)).days + 1
+    datesB = [datetime.date(y1B, 1, 1) + datetime.timedelta(d)
               for d in range(ndaysB)]
     
-    depth_resolution = 0.1 # metre
+    eh = eh[2:] if eh[:2] == '0x' else eh
+    while len(eh) < 6:
+        eh = '0' + eh
+    d1, d2, d3 = eh[:2], eh[:4], eh[:6]
+    outdir = os.path.join(prefix2, d1, d2, d3, 
+                          'EUR-11_%s_%s-%s_%s_%s0101-%s1231' % (
+                              m1, exA, exB, m2, y1A, y2B))
+    if not os.path.exists(outdir):
+        os.makedirs(outdir)
+    initp = os.path.join(outdir, 'Feb2015init')
+    parp = os.path.join(outdir, 'Feb2015par')
+    inputp = os.path.join(outdir, 'vanillainput')
+    mylakeinit(depth, area, initp)
+    mylakepar(longitude, latitude, parp)
+    mylakeinput(pA, pB, datesA, datesB, eh, inputp)
+    cmd = 'octave mylake.m %s %s %s %s %s %s' % (
+        initp, parp, inputp, y1A - 2, y2B, outdir)
+    print(cmd)
+    os.system(cmd)
+    for f in [initp, parp, inputp]:
+        os.system('bzip2 %s' % f)
+    expectedfiles = ['Feb2015init.bz2', 'Feb2015par.bz2', 'vanillainput.bz2', 
+                     'Tzt.csv.gz', 'Kzt.csv.gz', 'His.csv.gz', 'Qst.csv.gz']
+    flags = [os.path.exists(os.path.join(outdir, f)) for f in expectedfiles]
+    if all(flags):
+        with open(os.path.join(outdir, 'Feb2015COMPLETE'), 'w') as f:
+            f.write(datetime.datetime.now().isoformat())
+    ret = 0 if all(flags) else 100
+    return ret
+              
 
-    mylakeinit(depth, area, 'init')
-    mylakepar(longitude, latitude, 'par')
-    mylakeinput(pA, pB, datesA, datesB, eh, 'input')
 
+
+if __name__ == '__main__':
+    # model = 'EUR-11_ICHEC-EC-EARTH_historical_r1i1p1_KNMI-RACMO22E_v1_day'
+    # y1 = 1971
+    # latitude = 59.97
+    # longitude = 10.62
+    # eh = '1cb62'
+    # # eh = 'b516a3' ## a lake in the north
+    # area = 1100000
+    # depth = max(4.0, math.sqrt(area) / 100)
+    # y2 = y1 + 4
+    # y3 = y1 + 5
+    # y4 = y1 + 9
+    a = sys.argv
+    
     
